77dc601d02aff11ed60d67dbf2ed624616f22ea7
Richard Fenix <richard.fenix@gmail.com>
2017-08-31 14:03:52 -0400

    Updated View-related codes

--- public/js/pillbox.js
+++ public/js/pillbox.js
@@ -83,62 +83,78 @@ function medConflict(array){
           console.log(response);
           console.log(response.fullInteractionTypeGroup["0"].fullInteractionType.length);
           for (var i = 0; i < response.fullInteractionTypeGroup["0"].fullInteractionType.length; i++) {
-          $("#pillbox").append("<b>"+response.fullInteractionTypeGroup["0"].fullInteractionType[i].interactionPair["0"].description+"</b>")
+//          $("#pillbox").append("<b>"+response.fullInteractionTypeGroup["0"].fullInteractionType[i].interactionPair["0"].description+"</b>")
           }
 })};
 
   // This function constructs a schedule's HTML
-  
   function createNewRow(schedule) {
     // var formattedDate = new Date(schedule.createdAt);
     // formattedDate = moment(formattedDate).format("MMMM Do YYYY, h:mm:ss a");
-    var newSchedulePanel = $("<div>");
+    var newSchedulePanel = $("<div style='border:1px solid black'>");
 
 //    newSchedulePanel.addClass("panel panel-default");
 
-    newSchedulePanel.addClass("row col-md-12");
+    newSchedulePanel.addClass("row col-md-12 pillbox-table");
 
     var newSchedulePanelHeading = $("<div>");    
-    newSchedulePanelHeading.addClass("row col-md-12");
+    newSchedulePanelHeading.addClass("row panel-warning row col-md-12 pillbox-table");
 
     var deleteBtn = $("<button>");
-    deleteBtn.text("DELETE");
+    deleteBtn.text(" x ");
     deleteBtn.addClass("delete btn btn-danger");
     var editBtn = $("<button>");
     editBtn.text("EDIT");
     editBtn.addClass("edit btn btn-info");
-    var newScheduleMed_name = $("<div class='col-md-8'>");
+    var newScheduleMed_name = $("<div class='row panel-info col-md-2 pillbox-table'>");
 
+
+
+    var newSchedulePatientH3 = $("<h3>");
+
+    var newScheduleMed_NameH3 = $("<h3>");
 
     // var newScheduleDate = $("<small>");
-    var newScheduleDosage = $("<h3>");
-    var newScheduleSched = $("<h3>");
-    var newSchedulePatient = $("<div class='col-md-4'>");
-    newSchedulePatient.text("Patient: " + schedule.Patient.name);
-    newSchedulePatient.css({
-      float: "right",
-      color: "blue",
-      "margin-top":
-      "-10px"
-    });
+    var newScheduleDosage = $("<h4>");
+    var newScheduleSched = $("<h4>");
+    var newSchedulePatient = $("<div class='col-md-2'>");
+    newSchedulePatientH3.text(schedule.Patient.name);
+    // newSchedulePatient.css({
+    //   float: "right",
+    //   color: "blue",
+    //   "margin-top":
+    //   "-10px"
+    // });
 
-    var newSchedulePanelNotes = $("<div class=class='col-md-12'>");
-    newSchedulePanelNotes.addClass("panel-body");
-    var newScheduleNotes = $("<p>");
-    newScheduleMed_name.text(schedule.med_name + " ");
+    var newSchedulePanelNotes = $("<div class='row col-md-4 pillbox-table'>");
+    newSchedulePanelNotes.addClass("panel-body panel-warning");
+    var newScheduleNotes = $("<h4>");
+    newScheduleMed_NameH3.text(schedule.med_name + " ");
+
     medlist.push(schedule.med_code);
     newScheduleNotes.text(schedule.notes);
 
     newScheduleDosage.text(schedule.dosage + " ");
     newScheduleSched.text(schedule.sched + " ");
 //    var newScheduleImage = $("<img class='pill' src="+schedule.img_link+" style='width:304px;height:228px;'>");   
-    var newScheduleImage = $("<div class='col-md-2'> <img class='pillimg' src="+schedule.img_link+">");   
+    var newScheduleImage = $("<div class='col-md-2'> <img class='pill' src="+schedule.img_link+" style='width:100%;height:100%;'>");   
 
+    newSchedulePatient.append(newSchedulePatientH3);
+
+
+    newScheduleMed_name.append(newScheduleMed_NameH3);
     newScheduleMed_name.append(newScheduleDosage);
     newScheduleMed_name.append(newScheduleSched);
 
     newSchedulePanelHeading.append(newScheduleImage);
     newSchedulePanelHeading.append(newScheduleMed_name);
+
+    newSchedulePanelNotes.append(newScheduleNotes);
+
+    newSchedulePanelHeading.append(newSchedulePanelNotes);
+
+
+    newSchedulePanelHeading.append(newSchedulePatient);
 
     newSchedulePanelHeading.append(editBtn);
     newSchedulePanelHeading.append(deleteBtn);
@@ -146,11 +162,9 @@ function medConflict(array){
     // newSchedulePanelHeading.append(newScheduleMed_name);
     // newSchedulePanelHeading.append(newScheduleMed_name);
 
-
-    newSchedulePanelHeading.append(newSchedulePatient);
-    newSchedulePanelNotes.append(newScheduleNotes);
+//    newSchedulePanelNotes.append(newScheduleNotes);
     newSchedulePanel.append(newSchedulePanelHeading);
-    newSchedulePanel.append(newSchedulePanelNotes);
+//    newSchedulePanel.append(newSchedulePanelNotes);
 
     // newSchedulePanel.append(newScheduleImage);
 
@@ -159,74 +173,144 @@ function medConflict(array){
   }
 
 
-  function createNewRowBackup(schedule) {
-    // var formattedDate = new Date(schedule.createdAt);
-    // formattedDate = moment(formattedDate).format("MMMM Do YYYY, h:mm:ss a");
-    var newSchedulePanel = $("tr");
-    // newSchedulePanel.addClass("panel panel-default");
+//   function createNewRowBackup(schedule) {
+//     // var formattedDate = new Date(schedule.createdAt);
+//     // formattedDate = moment(formattedDate).format("MMMM Do YYYY, h:mm:ss a");
+//     var newSchedulePanel = $("<div>");
 
-    // convert this from div to table:
-    var newSchedulePanelHeading = $("");    
-    // newSchedulePanelHeading.addClass("panel-heading");
+// //    newSchedulePanel.addClass("panel panel-default");
 
-    // var newSchedulePanelHeading = $("<table>");    
-    // newSchedulePanelHeading.addClass("row col-md-6 col-md-offset-2 custyle text-center");
+//     newSchedulePanel.addClass("row col-md-12");
 
-    var deleteBtn = $("<button>");
-    deleteBtn.text("x");
-    deleteBtn.addClass("delete btn btn-danger");
-    var editBtn = $("<button>");
-    editBtn.text("EDIT");
-    editBtn.addClass("edit btn btn-info");
-    var newScheduleMed_name = $("<td>");
+//     var newSchedulePanelHeading = $("<div>");    
+//     newSchedulePanelHeading.addClass("row col-md-12");
 
-    // var newScheduleDate = $("<small>");
-    var newScheduleDosage = $("<td>");
-    var newScheduleSched = $("<td>");
-    var newSchedulePatient = $("<td>");
-    newSchedulePatient.text("Patient: " + schedule.Patient.name);
-    newSchedulePatient.css({
-      float: "right",
-      color: "blue",
-      "margin-top":
-      "-10px"
-    });
-
-    var newSchedulePanelNotes = $("<td>");
-    newSchedulePanelNotes.addClass("panel-body");
-    var newScheduleNotes = $("<td>");
-    newScheduleMed_name.text(schedule.med_name + " ");
-    medlist.push(schedule.med_code);
-    newScheduleNotes.text(schedule.notes);
-
-    newScheduleDosage.text(schedule.dosage + " ");
-    newScheduleSched.text(schedule.sched + " ");
-//    var newScheduleImage = $("<img class='pill' src="+schedule.img_link+" style='width:304px;height:228px;'>");   
-    // var newScheduleImage = $("<td><img class='pill' src="+schedule.img_link+" style='width:15%;height:25%;'>");   
-
-    newScheduleMed_name.append(newScheduleDosage);
-    newScheduleMed_name.append(newScheduleSched);
-
-//    newSchedulePanelHeading.append(newScheduleImage);
-    newSchedulePanelHeading.append(newScheduleMed_name);
-
-    // newSchedulePanelHeading.append(deleteBtn);
-    // newSchedulePanelHeading.append(editBtn);
-
-    // newSchedulePanelHeading.append(newScheduleMed_name);
-    // newSchedulePanelHeading.append(newScheduleMed_name);
+//     var deleteBtn = $("<button>");
+//     deleteBtn.text("DELETE");
+//     deleteBtn.addClass("delete btn btn-danger");
+//     var editBtn = $("<button>");
+//     editBtn.text("EDIT");
+//     editBtn.addClass("edit btn btn-info");
+//     var newScheduleMed_name = $("<div class='col-md-8'>");
 
 
-    newSchedulePanelHeading.append(newSchedulePatient);
-    newSchedulePanelNotes.append(newScheduleNotes);
-    newSchedulePanel.append(newSchedulePanelHeading);
-    newSchedulePanel.append(newSchedulePanelNotes);
+//     // var newScheduleDate = $("<small>");
+//     var newScheduleDosage = $("<h3>");
+//     var newScheduleSched = $("<h3>");
+//     var newSchedulePatient = $("<div class='col-md-4'>");
+//     newSchedulePatient.text("Patient: " + schedule.Patient.name);
+//     newSchedulePatient.css({
+//       float: "right",
+//       color: "blue",
+//       "margin-top":
+//       "-10px"
+//     });
 
-    // newSchedulePanel.append(newScheduleImage);
+//     var newSchedulePanelNotes = $("<div class=class='col-md-12'");
+//     newSchedulePanelNotes.addClass("panel-body");
+//     var newScheduleNotes = $("<p>");
+//     newScheduleMed_name.text(schedule.med_name + " ");
+//     medlist.push(schedule.med_code);
+//     newScheduleNotes.text(schedule.notes);
 
-    newSchedulePanel.data("schedule", schedule);
-    return newSchedulePanel;
-  }
+//     newScheduleDosage.text(schedule.dosage + " ");
+//     newScheduleSched.text(schedule.sched + " ");
+// //    var newScheduleImage = $("<img class='pill' src="+schedule.img_link+" style='width:304px;height:228px;'>");   
+//     var newScheduleImage = $("<div class='col-md-2'> <img class='pillimg' src="+schedule.img_link+" style='width:90%;height:90%;'>");   
+
+//     newScheduleMed_name.append(newScheduleDosage);
+//     newScheduleMed_name.append(newScheduleSched);
+
+//     newSchedulePanelHeading.append(newScheduleImage);
+//     newSchedulePanelHeading.append(newScheduleMed_name);
+
+//     newSchedulePanelHeading.append(editBtn);
+//     newSchedulePanelHeading.append(deleteBtn);
+
+//     // newSchedulePanelHeading.append(newScheduleMed_name);
+//     // newSchedulePanelHeading.append(newScheduleMed_name);
+
+
+//     newSchedulePanelHeading.append(newSchedulePatient);
+//     newSchedulePanelNotes.append(newScheduleNotes);
+//     newSchedulePanel.append(newSchedulePanelHeading);
+//     newSchedulePanel.append(newSchedulePanelNotes);
+
+//     // newSchedulePanel.append(newScheduleImage);
+
+//     newSchedulePanel.data("schedule", schedule);
+//     return newSchedulePanel;
+//   }
+
+
+//   function createNewRowBackup(schedule) {
+//     // var formattedDate = new Date(schedule.createdAt);
+//     // formattedDate = moment(formattedDate).format("MMMM Do YYYY, h:mm:ss a");
+//     var newSchedulePanel = $("tr");
+//     // newSchedulePanel.addClass("panel panel-default");
+
+//     // convert this from div to table:
+//     var newSchedulePanelHeading = $("");    
+//     // newSchedulePanelHeading.addClass("panel-heading");
+
+//     // var newSchedulePanelHeading = $("<table>");    
+//     // newSchedulePanelHeading.addClass("row col-md-6 col-md-offset-2 custyle text-center");
+
+//     var deleteBtn = $("<button>");
+//     deleteBtn.text("x");
+//     deleteBtn.addClass("delete btn btn-danger");
+//     var editBtn = $("<button>");
+//     editBtn.text("EDIT");
+//     editBtn.addClass("edit btn btn-info");
+//     var newScheduleMed_name = $("<td>");
+
+//     // var newScheduleDate = $("<small>");
+//     var newScheduleDosage = $("<td>");
+//     var newScheduleSched = $("<td>");
+//     var newSchedulePatient = $("<td>");
+//     newSchedulePatient.text("Patient: " + schedule.Patient.name);
+//     newSchedulePatient.css({
+//       float: "right",
+//       color: "blue",
+//       "margin-top":
+//       "-10px"
+//     });
+
+//     var newSchedulePanelNotes = $("<td>");
+//     newSchedulePanelNotes.addClass("panel-body");
+//     var newScheduleNotes = $("<td>");
+//     newScheduleMed_name.text(schedule.med_name + " ");
+//     medlist.push(schedule.med_code);
+//     newScheduleNotes.text(schedule.notes);
+
+//     newScheduleDosage.text(schedule.dosage + " ");
+//     newScheduleSched.text(schedule.sched + " ");
+// //    var newScheduleImage = $("<img class='pill' src="+schedule.img_link+" style='width:304px;height:228px;'>");   
+//     // var newScheduleImage = $("<td><img class='pill' src="+schedule.img_link+" style='width:15%;height:25%;'>");   
+
+//     newScheduleMed_name.append(newScheduleDosage);
+//     newScheduleMed_name.append(newScheduleSched);
+
+// //    newSchedulePanelHeading.append(newScheduleImage);
+//     newSchedulePanelHeading.append(newScheduleMed_name);
+
+//     // newSchedulePanelHeading.append(deleteBtn);
+//     // newSchedulePanelHeading.append(editBtn);
+
+//     // newSchedulePanelHeading.append(newScheduleMed_name);
+//     // newSchedulePanelHeading.append(newScheduleMed_name);
+
+
+//     newSchedulePanelHeading.append(newSchedulePatient);
+//     newSchedulePanelNotes.append(newScheduleNotes);
+//     newSchedulePanel.append(newSchedulePanelHeading);
+//     newSchedulePanel.append(newSchedulePanelNotes);
+
+//     // newSchedulePanel.append(newScheduleImage);
+
+//     newSchedulePanel.data("schedule", schedule);
+//     return newSchedulePanel;
+//   }
 
   // This function figures out which schedule we want to delete and then calls deleteSchedule
   function handleScheduleDelete() {
