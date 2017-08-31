9e03beb763efe14ebbaed5cf4017c24fb3323f92
Richard Fenix <richard.fenix@gmail.com>
2017-08-30 23:16:33 -0400

    Merge version by RF

--- server.js
+++ server.js
@@ -27,9 +27,6 @@ app.use(express.static("public"));
 
 // Routes
 // =============================================================
-// require("./routes/html-routes.js")(app);
-// require("./routes/author-api-routes.js")(app);
-// require("./routes/post-api-routes.js")(app);
 require("./routes/html-routes.js")(app);
 require("./routes/patient-api-routes.js")(app);
 require("./routes/schedule-api-routes.js")(app);
@@ -37,7 +34,7 @@ require("./routes/users-api-routes.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync({ force: true }).then(function() {
+db.sequelize.sync({ force: false }).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
